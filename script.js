ef9bf8432e2a01351d660cda53bf867c8623309a
ricardo-719 <teacherrickyricardo@gmail.com>
2023-01-17 10:18:07 +0700

    Add function splitting the link for styling

---
+++ script.js
@@ -0,0 +1,15 @@
+const enhance = id => {
+    const element = document.getElementById(id),
+        text = element.innerText.split("");
+
+
+    element.innerText = "";
+
+    text.forEach(letter => {
+        const span = document.createElement("span");
+        span.className="letter";
+        span.innerText=letter;
+        element.appendChild(span);
+    })
+    enhance("explosiveLink");
+}
